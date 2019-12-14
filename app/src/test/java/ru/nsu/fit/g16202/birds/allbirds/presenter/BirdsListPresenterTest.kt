a47470aaed553fa5ed7728482b69b422c65e31ea
pleshkovvli <pleshkovvli@gmail.com>
2019-12-15 01:09:49 +0700

    Add presenters tests.

---
+++ app/src/test/java/ru/nsu/fit/g16202/birds/allbirds/presenter/BirdsListPresenterTest.kt
@@ -0,0 +1,75 @@
+package ru.nsu.fit.g16202.birds.allbirds.presenter
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import ru.nsu.fit.g16202.birds.allbirds.interactor.BirdsInteractor
+import ru.nsu.fit.g16202.birds.allbirds.view.BirdsView
+import ru.nsu.fit.g16202.birds.bird.imagehandler.ImageHandler
+import ru.nsu.fit.g16202.birds.bird.interactor.BirdInteractor
+import ru.nsu.fit.g16202.birds.bird.view.BirdView
+
+class BirdsListPresenterTest {
+
+    @Test
+    fun bindViewWithInteractor() {
+        val sizeValue = 2345
+
+        val interactor = object : BirdsInteractor {
+            override val size: Int
+                get() = sizeValue
+
+            override fun createBirdInteractor(position: Int): BirdInteractor {
+                throw NotImplementedError()
+            }
+        }
+
+        val view = object : BirdsView {
+            var onBindlistener: ((BirdView, Int) -> Unit)? = null
+
+            override fun setOnBindBirdViewListener(
+                listener: ((BirdView, Int) -> Unit)?
+            ) {
+                onBindlistener = listener
+            }
+
+            override var itemCount: () -> Int = { -1 }
+        }
+
+
+        var listenerHadBeenCalled = false
+        val presenter = BirdsListPresenter(interactor, view) { _, _ ->
+            listenerHadBeenCalled = true
+        }
+
+        assertEquals(view.itemCount(), -1)
+
+        presenter.bindViewWithInteractor()
+        view.onBindlistener!!.invoke(object : BirdView {
+            override var fillView: (() -> Unit)? = null
+            override var name: String = ""
+            override var description: String = ""
+
+            override fun getImageHandler(): ImageHandler
+                    = throw NotImplementedError()
+
+            override fun setOnPlaySoundListener(
+                listener: (() -> Unit)?
+            ) = throw NotImplementedError()
+
+            override fun setOnStopSoundListener(
+                listener: (() -> Unit)?
+            ) = throw NotImplementedError()
+
+            override fun loadSound() = throw NotImplementedError()
+            override suspend fun playSound() = throw NotImplementedError()
+
+            override fun stopSound() = throw NotImplementedError()
+            override fun showImage() = throw NotImplementedError()
+
+        }, 1)
+
+        assertTrue(listenerHadBeenCalled)
+        assertEquals(view.itemCount(), sizeValue)
+    }
+}
---
+++ app/src/test/java/ru/nsu/fit/g16202/birds/bird/presenter/BirdElementPresenterTest.kt
@@ -0,0 +1,172 @@
+package ru.nsu.fit.g16202.birds.bird.presenter
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+import org.junit.Assert.*
+import ru.nsu.fit.g16202.birds.allbirds.repository.MockedBirdsRepository
+import ru.nsu.fit.g16202.birds.bird.entity.Bird
+import ru.nsu.fit.g16202.birds.bird.imagehandler.ImageHandler
+import ru.nsu.fit.g16202.birds.bird.imagehandler.ImageLoader
+import ru.nsu.fit.g16202.birds.bird.interactor.BirdInteractor
+import ru.nsu.fit.g16202.birds.bird.view.BirdView
+
+class BirdElementPresenterTest {
+
+    @Test
+    fun bindViewWithInteractor() {
+        val oneBird = MockedBirdsRepository.birds[2]
+
+        val interactor = object : BirdInteractor {
+            var imageLoaderSet = false
+            var imageLoaded = false
+
+            var onPlayListenerSet = false
+            var onStopListenerSet = false
+            var onSoundLoaderListenerSet = false
+
+            var played = false
+            var stopped = false
+            var loaded = false
+
+            override val bird: Bird
+                get() = oneBird
+
+            override var imageLoader: ImageLoader = object : ImageLoader {
+                    override fun loadImage(uri: String) {
+                        throw NotImplementedError()
+                    }
+                }
+                set(value) {
+                    field = value
+                    imageLoaderSet = true
+                }
+
+            override fun setOnPlayListener(listener: () -> Unit) {
+                onPlayListenerSet = true
+            }
+
+            override fun setOnStopListener(listener: () -> Unit) {
+                onStopListenerSet = true
+            }
+
+            override fun setOnSoundLoadedListener(listener: suspend () -> Unit) {
+                onSoundLoaderListenerSet = true
+            }
+
+            override fun playSound() {
+                played = true
+            }
+
+            override fun stopSound() {
+                stopped = true
+            }
+
+            override fun loadImage() {
+                loaded = true
+                imageLoader.loadImage("")
+            }
+        }
+
+        val view = object : BirdView {
+            var onPlayListenerSet = false
+            var onStopListenerSet = false
+
+            var imageShow = false
+
+            var played = false
+            var stopped = false
+            var loaded = false
+
+            override var fillView: (() -> Unit)? = null
+            override var name: String = ""
+            override var description: String = ""
+
+            override fun getImageHandler(): ImageHandler = object : ImageHandler {
+                override fun loadImage(uri: String) {
+                    interactor.imageLoaded = true
+                }
+
+                override fun showImage() {
+                    throw NotImplementedError()
+                }
+            }
+
+            override fun setOnPlaySoundListener(listener: (() -> Unit)?) {
+                onPlayListenerSet = true
+            }
+
+            override fun setOnStopSoundListener(listener: (() -> Unit)?) {
+                onStopListenerSet = true
+            }
+
+            override suspend fun playSound() {
+                played = true
+            }
+
+            override fun stopSound() {
+                stopped = true
+            }
+
+            override fun loadSound() {
+                loaded = true
+            }
+            override fun showImage() {
+                imageShow = true
+            }
+        }
+
+        val presenter = BirdElementPresenter(interactor, view)
+
+        assertFalse(interactor.imageLoaderSet)
+        assertFalse(interactor.imageLoaded)
+        assertFalse(interactor.onPlayListenerSet)
+        assertFalse(interactor.onStopListenerSet)
+        assertFalse(interactor.onSoundLoaderListenerSet)
+        assertFalse(interactor.played)
+        assertFalse(interactor.stopped)
+        assertFalse(interactor.loaded)
+
+        assertFalse(view.onPlayListenerSet)
+        assertFalse(view.onStopListenerSet)
+        assertFalse(view.imageShow)
+        assertFalse(view.played)
+        assertFalse(view.stopped)
+        assertFalse(view.loaded)
+
+        presenter.bindViewWithInteractor()
+        view.fillView!!()
+
+
+        assertEquals(oneBird.description, view.description)
+        assertEquals(oneBird.speciesName, view.name)
+
+        assertTrue(interactor.imageLoaderSet)
+        assertTrue(interactor.imageLoaded)
+        assertTrue(interactor.onPlayListenerSet)
+        assertTrue(interactor.onStopListenerSet)
+        assertTrue(interactor.onSoundLoaderListenerSet)
+
+        interactor.playSound()
+        assertTrue(interactor.played)
+
+        interactor.stopSound()
+        assertTrue(interactor.stopped)
+
+        interactor.loadImage()
+        assertTrue(interactor.loaded)
+
+        assertTrue(view.onPlayListenerSet)
+        assertTrue(view.onStopListenerSet)
+        assertTrue(view.imageShow)
+
+        runBlocking { view.playSound() }
+        assertTrue(view.played)
+
+        view.stopSound()
+        assertTrue(view.stopped)
+
+        view.loadSound()
+        assertTrue(view.loaded)
+    }
+}
